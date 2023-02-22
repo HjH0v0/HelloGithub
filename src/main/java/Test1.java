aa73c441aae5b3cbe87d93348f56cca87a0862ca
HjH0v0 <112875595+HjH0v0@users.noreply.github.com>
2023-02-22 20:54:33 +0800

    Add files via upload

---
+++ src/main/java/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("啦啦啦");
+        System.out.println("啦啦啦");
+        System.out.println("123");
+        System.out.println("456");
+    }
+}
